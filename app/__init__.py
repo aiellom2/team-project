fd486d3f1359128009cb469ba522030ca058dbaa
aiellom2 <aiellom2@southernct.edu>
2025-02-28 18:34:32 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 
-# Initialize app and db
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
 
 # Environment configuration (if needed)
 load_dotenv('.flaskenv')
 DB_NAME = os.environ.get('SQLITE_DB')
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+# Initialize app and db
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'csc330'
+
 
 # Database configuration
 DB_CONFIG_STR = 'sqlite:///' + os.path.join(basedir, DB_NAME)
@@ -21,4 +24,4 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 # Import routes and models
-from main import routes, models  # Adjust if necessary
+from app.routes import routes, models  # Adjust if necessary
