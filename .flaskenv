5919cfd49bb61bacc335f086ae6a8208480628bc
aiellom2 <aiellom2@southernct.edu>
2025-02-28 18:18:00 -0500

    edits

--- .flaskenv
+++ .flaskenv
@@ -1,4 +1,4 @@
-export FLASK_APP=routes.py
+export FLASK_APP=main.py
 export FLASK_DEBUG=1
 export FLASK_RUN_PORT=8080
 export FLASK_RUN_HOST=0.0.0.0
--- models.py
+++ models.py
@@ -1,6 +1,5 @@
-import db  # Import db from db/__init__.py
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    id = Column(db.Integer, primary_key=True)
+    username = Column(String(80), unique=True, nullable=False)
+    password = Column(String(120), nullable=False)
