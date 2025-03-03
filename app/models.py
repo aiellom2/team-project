0c830e10bfad05e18e9980eaf079168388418d7d
aiellom2 <aiellom2@southernct.edu>
2025-03-02 20:16:06 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password_hash = db.Column(db.String(150), nullable=False)
-    role = db.Column(db.String(20), nullable=False, default='employee')  # 'employee', 'manager', 'admin'
+    role = db.Column(db.String(20), nullable=False)  # 'employee', 'manager', 'admin'
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
