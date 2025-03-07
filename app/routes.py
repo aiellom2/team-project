30eff97d1bab9ab605f45c45053c10e701067799
aiellom2 <aiellom2@southernct.edu>
2025-03-07 17:01:27 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app
 from flask import render_template, request, redirect, url_for, flash
 from app.forms import AdminLoginForm, EmployeeLoginForm, ManagerLoginForm, RequestTypeForm
 from app import db
-from app.models import User
+from app.models import User, RequestType
 import sys
 
 
