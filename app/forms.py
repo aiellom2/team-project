6bae7324514340a1e48987610e339b8b42206ee8
aiellom2 <aiellom2@southernct.edu>
2025-03-07 17:37:41 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, IntegerField, SubmitField, EmailField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Email
 
 
 class AdminLoginForm(FlaskForm):
